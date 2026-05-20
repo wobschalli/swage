4466cb08439666cc412374cb1f72a8c321c5456c
Ian Wobschall <ian.wobschall@gmail.com>
2026-05-20 17:55:42 -0400

    updated swage installer to run without errors

--- README.md
+++ README.md
@@ -1,13 +1,10 @@
-# Flex
+# Swage
 :0
 
 A gem gluing together [Phlex](https://github.com/yippee-fun/phlex-rails/), [RubyUI](https://github.com/ruby-ui/ruby_ui), and [Superform](https://github.com/rubymonolith/superform/) via [Phlexible](https://github.com/joelmoss/phlexible) and a lot of monkeypatching.
 
-It currently replaces the scaffold generator but does not perform all of its functionality (such as migrations or routes).
 
 ## TODO
-* Fix the monkeypatching in `app/components/form.rb` to properly combine all of the attributes for the special fields
-* Find a way to hook into the scaffold generator so the default generator just calls the new generators for the proper templates
 * Find a way to copy a bunch of the necessary files, such as `app/controllers/application_controller.rb` or `app/views/base.rb` so that they don't get overridden when installing Superform or RubyUI
 * Actually make it into a gem by adding the proper files and removing the unnecessary ones
     * Also adding an install script would be really good
--- lib/generators/swage/install/install_generator.rb
+++ lib/generators/swage/install/install_generator.rb
@@ -4,52 +4,61 @@ module Swage::Generators
   class InstallGenerator < ::Rails::Generators::Base
     source_root File.expand_path("templates", __dir__)
 
-    deps = %w[ phlex-rails phlexible superform ruby_ui ]
-
-    deps.each do |d|
-      say "checking for dependency #{d} in the gemfile"
-      if gem_in_bundle?(d)
-        say "found!"
-        deps.delete d
-      else
-        say "missing #{d} in gemfile"
-      end
-    end
-
-    if yes?("Would you like to install the missing dependencies automatically?")
-      add_source "https://rubygems.org" do
-        deps.each do |d|
-          gem d, group: d == "ruby_ui" ? :development : %i[ development, testing, production ]
+    def add_dependencies
+      deps = %W[ tailwindcss-rails phlex-rails phlexible superform ruby_ui ]
+      missing = []
+      deps.each do |d|
+        say "checking for dependency #{d} in the gemfile"
+        if Gem.loaded_specs.has_key?(d)
+          say "found!"
+        else
+          say "missing #{d} in gemfile"
+          missing << d
         end
       end
-    else
-      exit 1
+
+      if missing.any?
+        if yes?("would you like to automatically install the missing dependencies?")
+          add_source "https://rubygems.org" do
+            deps.each do |d|
+              gem d, group: d == "ruby_ui" ? :development : %i[ development, testing, production ]
+            end
+          end
+        else
+          exit 1
+        end
+      end
+
+      say "install tailwind"
+      execute_command "rake", "tailwindcss:install"
+
+      say "install phlex"
+      generate "phlex:install"
+
+      say "install superform"
+      generate "superform:install"
+
+      say "install rubyui"
+      generate "ruby_ui:install"
+      generate "ruby_ui:component:all"
+
+      say "install swage"
     end
 
-    say "install phlex"
-    generate "phlex:install"
-
-    say "install superform"
-    generate "superform:install"
-
-    say "install rubyui"
-    generate "ruby_ui:install"
-    generate "ruby_ui:component:all"
-
     def create_initializer
-      template "swage.rb.tt", File.join(destination_root, "config/initializers/swage.rb")
+      template "swage.rb.tt", File.join(destination_root, "config/initializers/swage.rb"), force: true
     end
 
     def create_base_component
-      template "base_component.rb.tt", File.join(destination_root, "app/components/base.rb")
+      template "base_component.rb.tt", File.join(destination_root, "app/components/base.rb"), force: true
     end
 
     def create_base_view
-      template "base_view.rb.tt", File.join(destination_root, "app/views/base.rb")
+      template "base_view.rb.tt", File.join(destination_root, "app/views/base.rb"), force: true
     end
 
     def create_base_form
-      template "form.rb.tt", File.join(destination_root, "app/components/form.rb")
+      template "form.rb.tt", File.join(destination_root, "app/components/form.rb"), force: true
     end
   end
 end
